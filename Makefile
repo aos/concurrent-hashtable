496b233fc84775ec323d1e0c85d48f6ae0ac7de0
Aos <aosdab@gmail.com>
2017-09-05 23:13:02 -0400

    Separated worker thread

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 CC=gcc
 CFLAGS=-c -Wall 
-OBJECTS=table.o table_funcs.o
+OBJECTS=table.o table_funcs.o worker.o
 
 table: $(OBJECTS)
 	$(CC) $(OBJECTS) -o table -O3 -lpthread
 
 clean:
-	rm -f table table_funcs.o table.o
+	rm -f table *.o 
--- table.c
+++ table.c
@@ -7,38 +7,6 @@
 
 #include "table.h"
 
-void *worker(void *value) {
-  uint64_t tid;
-  pthread_threadid_np(NULL, &tid);
-
-  // Count # of lines
-  long counter = 0;
-
-  // Open file
-  FILE *fp = fopen((char*) value, "r");
-
-  if (fp == NULL) {
-    printf("Open failed\n");
-  }
-
-  // Initialize line pointer, length, and line capacity
-  char *line = NULL;
-  size_t linecap = 0;
-  ssize_t linelen;
-
-  while ((linelen = getline(&line, &linecap, fp)) > 0) {
-    insert(line);
-    char *string = find(line);
-
-    counter++;
-  }
-  
-  fclose(fp);
-
-  printf("Thread (%llu) -> file: %s, words: %ld\n", tid, (char *) value, counter);
-  return (void*) counter;
-}
-
 int
 main(int argc, char *argv[])
 {
